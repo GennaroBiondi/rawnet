c64d6b273dd28c389be2a6ddda23fc4bdb049a5c
GennaroBiondi <throwawayishere@ultrakill.com>
2026-08-19 22:49:00 +0200

    fix: rename socket.rs to socket_trait.rs

--- src/socket/mod.rs
+++ src/socket/mod.rs
@@ -1,7 +1,7 @@
 mod arp_socket;
 mod local_socket;
-mod socket;
+mod socket_trait;
 
 pub use arp_socket::ArpSocket;
 pub use local_socket::LocalSocket;
-pub use socket::*;
+pub use socket_trait::*;
