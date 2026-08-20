46d07fb9ef51283a2746f2bdb39e2912bdd87b43
GennaroBiondi <throwawayishere@ultrakill.com>
2026-08-20 17:31:45 +0200

    fix(ArpSocket): make ArpHeader and the as_packet method public

--- src/socket/mod.rs
+++ src/socket/mod.rs
@@ -2,6 +2,6 @@ mod arp_socket;
 mod local_socket;
 mod socket_trait;
 
-pub use arp_socket::ArpSocket;
+pub use arp_socket::{ArpHeader, ArpSocket};
 pub use local_socket::LocalSocket;
 pub use socket_trait::*;
